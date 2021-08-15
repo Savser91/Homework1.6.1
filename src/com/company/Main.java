e90940e10033cd85cd074af468ec78f8df41768b
Savser91 <savser91@gmail.com>
2021-08-15 14:28:32 +0300

    Версия 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Book book1 = new Book("Евгений Онегин", "Александр Пушкин", 150);
+        Book book2 = new Book("Дубровский", "Александр Пушкин", 100);
         System.out.println(book1.toString());
+        System.out.println(book2.toString());
         book1.Print();
+        book2.Print();
     }
 }
